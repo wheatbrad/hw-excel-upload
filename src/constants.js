44db9fb25cf8fe3d99a0d05c44c6b5d2f08c5bb1
Brad Runnels <brad@ocozzio.com>
2025-03-07 10:13:40 -0500

    remove state enum

--- src/constants.js
+++ src/constants.js
@@ -12,18 +12,3 @@ export const API_ENDPOINT_ROOT = 'https://api.ocozzio.com/homewerks';
  */
 export const UPLOAD_EXPECTED_FILE_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
 
-/**
- * Enum defining possible drop zone states:
- *   - [0] READY  - Drop zone default state, nothing being dragged
- *   - [1] PROMPT - Highlight drop zone area for user
- *   - [2] ACCEPT - Over dropzone, file acceptable for processing
- *   - [3] WARN   - Over dropzone, file not acceptable for processing
- * 
- * @type {Record<string, number>}
- */
-export const DZ_STATE = Object.create(null, {
-  READY:  { value: 0 },
-  PROMPT: { value: 1 },
-  ACCEPT: { value: 2 },
-  WARN:   { value: 3 }
-});
