5ea30ffca8efcdffc23c0368b36f69e3897aaf42
Brad Runnels <runnels.brad@gmail.com>
2025-05-09 08:55:03 -0400

    changes for +Retailer feature

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
-createRoot(document.getElementById('hw-model-number-upload')).render(
+createRoot(document.getElementById('hw-model-sku-upload')).render(
   <StrictMode>
     <App />
   </StrictMode>
